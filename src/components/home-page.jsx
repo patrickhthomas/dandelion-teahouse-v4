21746aca53f3b0664a0f9afaf27822ff953a6988
Patrick <patrick@sendhelp.biz>
2022-01-08 21:37:44 -0700

    svg hero img

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -6,14 +6,17 @@ import Logo from "../icons/logo"
 import { Navigation } from "./navigation"
 import Wordmark from "../images/dandelion-wordmark"
 import { CartButton } from "./cart-button"
+import { HoursLocation } from "./hours-and-location"
 import {
   hero,
   intro,
   title,
   tagline,
-  container,
   wordmark,
+  heroBlurb,
   wordmarkContainer,
+  heroContainer,
+  container,
   logo as logoCss,
   searchButton,
   nav,
@@ -29,16 +32,20 @@ export function HomePage() {
   }, 0)
 
   return (
-    <div className={container}>
-      <Wordmark className={wordmark}/>
+    <>
+    <div className={heroContainer}>
       <div className={wordmarkContainer}>
-      
+      <Wordmark className={wordmark}/>
       </div>
       <h1 className={title}> Teahouse & Apothecary</h1>
       <h3 className={tagline}>Where all your wishes come true!</h3>
-      <p>We are a modern apothecary and teahouse located in downtown Vancouver, WA. Our shop features unique teas from all over the world, tea lattes, handcrafted herbal tisane blends, loose herbs, natural small batch skincare products made on-site by Gifts From the Earth, a rotating selection of art by local artists, and so much more. We are proud to be a women owned, inclusive company!</p>
+      <p className={heroBlurb}>We are a modern apothecary and teahouse located in downtown Vancouver, WA. Our shop features unique teas from all over the world, tea lattes, handcrafted herbal tisane blends, loose herbs, natural small batch skincare products made on-site by Gifts From the Earth, a rotating selection of art by local artists, and so much more. We are proud to be a women owned, inclusive company!</p>
       <caption>caption text</caption>
     </div>
+    <div className={container}>
+      <HoursLocation />
+    </div>
+    </>
 
   )
 }
---
+++ src/components/hours-and-location.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Clock from '../components/Icons/clock'
+import Pin from '../components/Icons/pin'
+
+import {
+  wrapper,
+  intro,
+  title,
+  tagline,
+  wordmark,
+  heroBlurb,
+  wordmarkContainer,
+  heroContainer,
+  container,
+  logo as logoCss,
+  searchButton,
+  nav,
+} from "./hours-and-location.module.css"
+
+
+
+
+const HoursLocation = (props) =>  {
+  return (
+    
+<div className={wrapper}>
+    <h2 className="sectionH2">Location</h2>
+    <div className='divider'>
+    <div className='location'>
+    <a target="_blank" rel="noopener noreferrer" href='https://goo.gl/maps/biekmuwE3pK9kq4o7'>
+    <h3 className={address}>109 W. 7th Street<br></br>Vancouver, WA</h3><p className="mapsLink">open with google maps &gt;</p>
+    </a>
+
+    </div>
+    <div className='hours'>
+    <h3 className="hoursTitle">Store Hours</h3>
+    <ul className={hours}>
+      <li className={day}>
+
+        <p>Monday</p>
+        <p>CLOSED</p>
+
+          </li>
+          <li className={day}>
+
+        <p>Tuesday</p>
+        <p>CLOSED</p>
+
+          </li>
+
+          </ul>
+          </div>
+          </div>
+          </div>
+          )
+        }
+        
+        export default HoursLocation
